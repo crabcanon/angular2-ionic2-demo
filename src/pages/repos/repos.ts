84a2a77629da689949f33bb859b092642d1d2b6b
Ye Huang <sysuhuangye@gmail.com>
2017-02-18 20:37:20 +0200

    Implement InfiniteScroll for Repos list.

--- src/pages/repos/repos.ts
+++ src/pages/repos/repos.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
-import { SQLite } from 'ionic-native';
-
-let db = new SQLite();
+import { SqliteService }  from '../../providers/sqlite-service';
 
 export interface RepoInterface {
   name: string;
@@ -21,16 +19,15 @@ export interface RepoInterface {
   templateUrl: 'repos.html'
 })
 export class ReposPage {
-  public repos: RepoInterface[];
+  public repos: RepoInterface[] = [];
+  private startNum: number = 1;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    private platform: Platform
+    private platform: Platform,
+    public sqliteService: SqliteService
   ) {
-    this.repos = [
-      { name: 'Unknown', description: 'Unknown' }
-    ];
     platform.ready().then(() => {
       this.openDB();
     });
@@ -46,31 +43,34 @@ export class ReposPage {
   }
 
   openDB() {
-    db.openDatabase({ 
-      name: 'data.db',
-      location: 'default' 
-    })
-    .then(() => {
-      this.loadData();
-    })
-    .catch((error) => {
+    this.sqliteService.openDatabase('data.db', 'default').then(() => {
+      this.loadRepos();
+    }, error => {
       console.log(error);
     });
   }
 
-  loadData() {
-    db.executeSql('SELECT * FROM repos_json1 LIMIT 1,50', []).then((data) => {
-      if (data.rows.length > 0) {
-        let repoDescription: string;
-        for(let i = 0; i < data.rows.length; i++) {
-          repoDescription = data.rows.item(i).description ? data.rows.item(i).description : 'Unknown';  
-          this.repos.push({
-            name: data.rows.item(i).name, 
-            description: repoDescription,
-            url: data.rows.item(i).htmlurl
-          });
-        }
-      }
+  loadRepos() {
+    return new Promise((resolve, reject) => {
+      this.sqliteService.getRepos(this.startNum).subscribe((data) => {
+        console.log(data);
+        this.repos = this.repos.concat(data);
+      }, (error) => {
+        console.log('Loading Repos Error: ', error);
+        reject(error);
+      }, () => {
+        console.log('Loading Repos Finished!');
+        resolve(true);
+      });
     });
+  }
+
+  loadMoreRepos(infiniteScroll:any) {
+     console.log('Loading More Repos: startNum is currently ' + this.startNum);
+     this.startNum += 50;
+     
+     this.loadRepos().then(() => {
+       infiniteScroll.complete();
+     });
   }
 }
