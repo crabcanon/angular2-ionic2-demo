97c058eefe1df1b9308ec1ad779645d99c6c28c5
Ye Huang <sysuhuangye@gmail.com>
2017-02-25 19:04:20 +0200

    Create global variables

---
+++ src/global.ts
@@ -0,0 +1,10 @@
+export const GLOBALS = Object.freeze({
+  AUTH0_OAUTH_URL: 'https://tieto-upm.eu.auth0.com/oauth/ro',
+  AUTH0_TOKENINFO_URL: 'https://tieto-upm.eu.auth0.com/tokeninfo',
+  AUTH0_DELEGATION_URL: 'https://tieto-upm.eu.auth0.com/delegation',
+  AUTH0_CLIENT_ID: '8wP6CKrG7YAC1ggU1syVlrJOGyjPU4VE',
+  AUTH0_CONNECTION: 'Username-Password-Authentication',
+  AUTH0_SCOPE: 'openid',
+  FIREBASE_BASE_URL: 'https://ionic2-sqlite-firebase.firebaseio.com',
+  FIREBASE_SCOPE: 'openid name email displayName'
+});
