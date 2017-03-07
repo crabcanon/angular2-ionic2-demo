7b2ce78ff0c95895b34c2b049b1ab2a928f66e55
Ye Huang <sysuhuangye@gmail.com>
2017-03-07 20:51:23 +0200

    Add Firebase CURD methods for Camera Tab

--- src/providers/firebase-service.ts
+++ src/providers/firebase-service.ts
@@ -19,7 +19,9 @@ import {
 export class FirebaseService {
   private authState: FirebaseAuthState;
   private users: FirebaseListObservable<any>;
+  private images: FirebaseListObservable<any>;
   private user: any;
+  private image: any;
 
   constructor(
     public auth$: AngularFireAuth,
@@ -47,22 +49,42 @@ export class FirebaseService {
 
   createUser(userInfo: any) {
     this.user = this.af.database.object(`/users/${userInfo.uid}/profile`);
-    this.user.set(userInfo);
+    return this.user.set(userInfo);
   }
 
   updateUser(key: string, userInfo: any) {
     this.user = this.af.database.object(`/users/${key}/profile`);
-    this.user.update(userInfo);
+    return this.user.update(userInfo);
   }
 
   deleteUser(key: string) {
     this.user = this.af.database.object(`/users/${key}`); 
-    this.user.remove();
+    return this.user.remove();
   }
 
   deleteAllUsers() {
     this.users = this.af.database.list('/users');
-    this.users.remove();
+    return this.users.remove();
+  }
+
+  createImage(userKey: string, imageObj: any) {
+    this.image = this.af.database.object(`/users/${userKey}/images/${imageObj.id}`);
+    return this.image.set(imageObj);
+  }
+
+  updateImage(userKey: string, imageKey: string, imageObj: any) {
+    this.image = this.af.database.object(`/users/${userKey}/images/${imageKey}`);
+    return this.image.update(imageObj);
+  }
+
+  deleteImage(userKey: string, imageKey: string) {
+    this.image = this.af.database.object(`/users/${userKey}/images/${imageKey}`);
+    return this.image.remove();
+  }
+
+  deleteAllImages(userKey: string) {
+    this.images = this.af.database.list(`/users/${userKey}/images`);
+    return this.images.remove();
   }
 
 }
