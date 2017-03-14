3ec32a76b1f8187e3fda142d02e6b94e2a5c75f4
Ye Huang <sysuhuangye@gmail.com>
2017-03-14 15:18:03 +0200

    Invoke camera and file system for selecting and reading images.

--- src/providers/native-service.ts
+++ src/providers/native-service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Events } from 'ionic-angular'; 
-import { BatteryStatus, Device, Network } from 'ionic-native';
+import { Events, Platform } from 'ionic-angular'; 
+import { BatteryStatus, Device, Network, Camera, File, Transfer, FilePath } from 'ionic-native';
+import { Observable } from 'rxjs/Rx';
+
+declare var cordova: any;
 
 export interface DeviceInfoInterface {
   uuid?: string,
@@ -30,7 +33,7 @@ export class NativeService {
   public networkDisconnectSubscription: any;
 
 
-  constructor(public events: Events) {
+  constructor(public events: Events, public platform: Platform) {
     console.log('Hello Native Provider');
   }
 
@@ -94,6 +97,89 @@ export class NativeService {
     this.batteryOnCriticalSubscription.unsubscribe();
     this.networkConnectSubscription.unsubscribe();
     this.networkDisconnectSubscription.unsubscribe();
+  }
+
+
+  parseImageAsString(sourceType) {
+    let options = this.setOptions(sourceType);
+    let currentName: string = null;
+    let currentPath: string = null;
+
+    return Observable.create((observer) => {
+      if (this.platform.is('android') && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+        Camera.getPicture(options).then(imagePath => {
+          return imagePath; 
+        }).catch(error => {
+          console.log('Camera GetPicture Error: ', JSON.stringify(error));
+          observer.error(error);
+        }).then(value => {
+          currentName = value.substring(value.lastIndexOf('/') + 1, value.lastIndexOf('?'));
+          return FilePath.resolveNativePath(value);
+        }).then(filePath => {
+          currentPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+          console.log('CurrentName, CurrentPath: ', currentName, currentPath);
+          return this.convertImageAsBase64(currentPath, currentName);
+        }).catch(error => {
+          console.log('FilePath Resolve Android Native Path Error: ', JSON.stringify(error));
+          observer.error(error);
+        }).then(imageString => {
+          observer.next(imageString);
+          observer.complete();
+        });
+      } else {
+        Camera.getPicture(options).then(imagePath => {
+          return imagePath; 
+        }).catch(error => {
+          console.log('Camera GetPicture Error: ', JSON.stringify(error));
+          observer.error(error);
+        }).then(value => {
+          currentName = value.substr(value.lastIndexOf('/') + 1);
+          currentPath = value.substr(0, value.lastIndexOf('/') + 1);
+          console.log('CurrentName, CurrentPath: ', currentName, currentPath);
+          return this.convertImageAsBase64(currentPath, currentName);
+        }).then(imageString => {
+          observer.next(imageString);
+          observer.complete();
+        });
+      }
+    });
+  }
+
+  setOptions(srcType: number) {
+    let options = {
+      quality: 50,
+      targetWidth: 300,
+      targetHeight: 300,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: srcType,
+      encondingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      allowEdit: true,
+      correctOrientation: true
+    }
+    return options;
+  }
+
+  // Create a new name for the image
+  createFileName() {
+    let d = new Date();
+    return `${d.getTime()}.jpg`;
+  }
+
+  // Copy the image to a local folder
+  copyFileToLocalDir(filePath, fileName, newFileName) {
+    return File.copyFile(filePath, fileName, cordova.file.dataDirectory, newFileName);
+  }
+
+  // Convert the image as a base64 format string
+  convertImageAsBase64(filePath, fileName) {
+    console.log('File path and name: ', filePath, fileName);
+    return File.readAsDataURL(filePath, fileName);
+  }
+
+  // Always get the accurate path to your apps folder
+  pathForImage(img) {
+    return img ? cordova.file.dataDirectory + img : '';
   }
 
 }
